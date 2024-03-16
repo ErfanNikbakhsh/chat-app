e4d08153ba06577cebe4632c6640137dd582a6cd
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-03-16 10:13:46 +0330

    INITIALIZE the server with http & websocket protocols

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const app = express();
+const http = require('http');
 const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('New websocket connection');
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port:${port}`);
 });
