90ca60a24473d9ef57b4ff39e60b9ead8c85b491
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-03-19 13:21:25 +0330

    DEFINE sendMessage, sendLocation & disconnect eventListeners & send some messages

--- src/index.js
+++ src/index.js
@@ -11,8 +11,27 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-io.on('connection', () => {
+let count = 0;
+
+io.on('connection', (socket) => {
   console.log('New websocket connection');
+
+  socket.emit('message', 'Welcome!');
+
+  socket.broadcast.emit('message', 'A new user has joined!');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('sendLocation', (location) => {
+    const googleMapsLink = `https://google.com/maps?q=${location.latitude},${location.longitude}`;
+    io.emit('message', googleMapsLink);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!');
+  });
 });
 
 server.listen(port, () => {
