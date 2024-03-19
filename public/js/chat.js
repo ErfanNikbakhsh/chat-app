14b3ec6545176f73728bbeed7499d237fa315345
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-03-19 13:23:01 +0330

    ADD message eventListener & send sendMessage & sendLocation events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,43 @@
-io();
+const socket = io();
+
+socket.on('message', (message) => {
+  console.log(message);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let message = e.target.elements.message.value;
+
+  socket.emit('sendMessage', message);
+  message = '';
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) return alert('Geolocation is not supported by your browser.');
+
+  navigator.geolocation.getCurrentPosition(showPosition, showError);
+});
+
+const showPosition = (position) => {
+  const location = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  };
+
+  socket.emit('sendLocation', location);
+};
+
+const showError = (error) => {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      console.log('User denied the request for Geolocation.');
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log('Location information is unavailable.');
+      break;
+    case error.TIMEOUT:
+      console.log('The request to get user location timed out.');
+      break;
+  }
+};
