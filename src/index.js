e0039b0f2a5ddb4efcb4cee951d3fb748beb989e
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-03-22 03:47:50 +0330

    FILTERING bad-words & use functions for generating the data that would be sent

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const { Server } = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,21 +18,31 @@ let count = 0;
 io.on('connection', (socket) => {
   console.log('New websocket connection');
 
-  socket.emit('message', 'Welcome!');
+  socket.emit('message', generateMessage('Welcome!'));
 
-  socket.broadcast.emit('message', 'A new user has joined!');
+  socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
-  socket.on('sendMessage', (message) => {
-    io.emit('message', message);
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+
+    io.emit('message', generateMessage(message));
+    callback();
   });
 
-  socket.on('sendLocation', (location) => {
+  socket.on('sendLocation', (location, callback) => {
     const googleMapsLink = `https://google.com/maps?q=${location.latitude},${location.longitude}`;
-    io.emit('message', googleMapsLink);
+
+    io.emit('locationMessage', generateLocationMessage(googleMapsLink));
+
+    callback();
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left!');
+    io.emit('message', generateMessage('A user has left!'));
   });
 });
 
